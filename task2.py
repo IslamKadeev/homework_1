75cf302e796f6632ae90570108d44dff03dfb98b
IslamKadeev <kadeevkgeu@gmail.com>
2022-06-25 23:15:03 +0300

    Задача №2

--- task2.py
+++ task2.py
@@ -1,12 +1,15 @@
 def int32_to_ip(int32):
     bytes_list = []
     ip = ''
+
     for i in range (4):
         bytes_list.append(int32 & 255)
         int32 >>= 8
+
     bytes_list.reverse()
     for byte in bytes_list:
         ip += str(byte) + "."
+        
     return ip[:-1]
 
 assert int32_to_ip(2154959208) == "128.114.17.104"
